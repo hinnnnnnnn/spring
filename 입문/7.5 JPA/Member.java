ddd6b7fb4fcab8b15b5afcf3b7c6b5371338bdf2
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-12 12:08:57 +0900

    Update Member.java

--- 입문/7.5 JPA/Member.java
+++ 입문/7.5 JPA/Member.java
@@ -1,5 +1,6 @@
-@Entity //추가
+⛓️‍💥JPA 엔티티 매핑
+@Entity ⭐️추가⭐️
 public class Member {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) //추가
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY) ⭐️추가⭐️
     private Long id; //식별자
