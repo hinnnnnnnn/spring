fb07a0b60c2b95b1961b593264486fa039c714e9
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-12 12:13:02 +0900

    Update SpringConfig.java

--- 입문/7.5 JPA/SpringConfig.java
+++ 입문/7.5 JPA/SpringConfig.java
@@ -1,29 +1,18 @@
-package hello.hello_spring;
-
-import hello.hello_spring.repository.JpaMemberRepository;
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
-import hello.hello_spring.repository.jdbcTemplateMemberRepository;
-import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
+⚙️ JPA를 사용하도록 스프링 설정 변경
+    
 @Configuration
 public class SpringConfig {
 
-
+    ⭐️추가⭐️
     private EntityManager em;
 
     @Autowired
     public SpringConfig(EntityManager em) {
         this.em = em;
     }
+    ⭐️추가 끝⭐️
 
-//    private DataSource dataSource;
+//    private DataSource dataSource; 주석처리
 //
 //    @Autowired
 //    public SpringConfig(DataSource dataSource) {
@@ -38,51 +27,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        //return new jdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
-}
-package hello.hello_spring;
-
-import hello.hello_spring.repository.JpaMemberRepository;
-import hello.hello_spring.repository.MemberRepository;
-import hello.hello_spring.repository.MemoryMemberRepository;
-import hello.hello_spring.repository.jdbcTemplateMemberRepository;
-import hello.hello_spring.service.MemberService;
-import jakarta.persistence.EntityManager;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
-
-@Configuration
-public class SpringConfig {
-
-
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
-        this.em = em;
-    }
-
-//    private DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-
-    @Bean
-    public MemberService memberService() {
-        return new MemberService(memberRepository());
-    }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        //return new MemoryMemberRepository();
-        //return new jdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
+        //return new jdbcTemplateMemberRepository(dataSource)
+        return new JpaMemberRepository(em); ⭐️추가⭐️
     }
 }
