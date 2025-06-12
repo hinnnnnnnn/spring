ab3f5b0ca42de4d6be045165c1163f75589758bc
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-12 12:07:40 +0900

    Update 환경 설정.java

--- 입문/7.5 JPA/환경 설정.java
+++ 입문/7.5 JPA/환경 설정.java
@@ -1,43 +1,19 @@
-build_gradle
-  
-plugins {
-	id 'java'
-	id 'org.springframework.boot' version '3.5.0'
-	id 'io.spring.dependency-management' version '1.1.7'
-}
-
-group = 'hello'
-version = '0.0.1-SNAPSHOT'
-
-java {
-	toolchain {
-		languageVersion = JavaLanguageVersion.of(17)
-	}
-}
-
-repositories {
-	mavenCentral()
-}
-
+⚙️ build.gradle 파일에 JPA, h2 데이터베이스 관련 라이브러리 추가
 dependencies {
 	implementation 'org.springframework.boot:spring-boot-starter-thymeleaf'
 	implementation 'org.springframework.boot:spring-boot-starter-web'
 	testImplementation 'org.springframework.boot:spring-boot-starter-test'
 	testRuntimeOnly 'org.junit.platform:junit-platform-launcher'
 //	implementation 'org.springframework.boot:spring-boot-starter-jdbc'
-	implementation 'org.springframework.boot:spring-boot-starter-data-jpa'
+	implementation 'org.springframework.boot:spring-boot-starter-data-jpa' ⭐️여기⭐️
 	runtimeOnly 'com.h2database:h2'
 }
 
-tasks.named('test') {
-	useJUnitPlatform()
-}
 
 
 
-
-application.properties
- 
+⚙️ 스프링 부트에 JPA 설정 추가
+📌`resources - templates - application.properties`
 spring.datasource.username=sa
 spring.jpa.show.sql=true
 spring.jpa.hibernate.ddl-auto=none
