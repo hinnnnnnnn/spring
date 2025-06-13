87eeda8f1d25dca0e22cd8025c6e6c2504fe689d
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-13 14:32:15 +0900

    Create SpringConfig.java

---
+++ 입문/7.6 스프링 데이터 JPA/SpringConfig.java
@@ -0,0 +1,50 @@
+package hello.hello_spring;
+
+import hello.hello_spring.repository.JpaMemberRepository;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import hello.hello_spring.repository.jdbcTemplateMemberRepository;
+import hello.hello_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SpringConfig {
+
+    private final MemberRepository memberRepository; //option + enter -> 생성자 매개변수 초기화
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+//    private EntityManager em;
+//
+//    @Autowired
+//    public SpringConfig(EntityManager em) {
+//        this.em = em;
+//    }
+
+//    private DataSource dataSource;
+//
+//    @Autowired
+//    public SpringConfig(DataSource dataSource) {
+//        this.dataSource = dataSource;
+//    }
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository);
+    }
+
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        //return new MemoryMemberRepository();
+//        //return new jdbcTemplateMemberRepository(dataSource);
+//        //return new JpaMemberRepository(em);
+//    }
+}
