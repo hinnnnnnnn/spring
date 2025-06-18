22a2bf18b1807a855bdec5799365b965f0c2001f
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-18 15:06:44 +0900

    Create MemberService.java

---
+++ 기본/2.4 회원 도메인 개발/MemberService.java
@@ -0,0 +1,11 @@
+⛓️‍💥hello.core - member - MemberService (인터페이스 생성)
+📌MemberService.java
+
+package hello.core.member;
+
+public interface MemberService {
+
+    void join(Member member);
+
+    Member findMember(Long memberId);
+}
