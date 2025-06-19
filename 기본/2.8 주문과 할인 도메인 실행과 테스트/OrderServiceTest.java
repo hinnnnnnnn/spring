559952eec0c2d5fafe302424c1245c670e917fa5
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-19 16:47:16 +0900

    Create OrderServiceTest.java

---
+++ 기본/2.8 주문과 할인 도메인 실행과 테스트/OrderServiceTest.java
@@ -0,0 +1,28 @@
+⛓️‍💥test - hello.core - order - OrderServiceTest (클래스 생성)
+📌OrderServiceTest.java
+
+package hello.core.order;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() { 
+        Long memberId = 1L;
+        Member member = new Member(memberId, "memberA", Grade.VIP); //ID 1L, 이름 "memberA", 등급 VIP인 회원을 생성 후 join() 메서드로 저장
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000); //해당 회원이 itemA 상품을 10,000원에 주문, OrderServiceImpl에서 회원 정보를 조회하고, 할인 정책 적용 후 주문 생성
+
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
