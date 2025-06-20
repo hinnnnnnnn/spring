f48155fbed76a175943e744af2e24c71c87107f0
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-20 16:00:58 +0900

    Update RateDiscountPolicy.java

--- 기본/2.9 새로운 할인 정책 개발/RateDiscountPolicy.java
+++ 기본/2.9 새로운 할인 정책 개발/RateDiscountPolicy.java
@@ -11,7 +11,7 @@ public class RateDiscountPolicy implements DiscountPolicy{
     @Override
     public int discount(Member member, int price) { //discount에서 command + shift + T : 새 테스트 생성
         if (member.getGrade() == Grade.VIP) {
-            return price * discountPolicy / 100;
+            return price * discountPolicy / 100; //price * 10 / 100 → 10% 할인을 계산해 반환
         } else {
             return 0;
         }
