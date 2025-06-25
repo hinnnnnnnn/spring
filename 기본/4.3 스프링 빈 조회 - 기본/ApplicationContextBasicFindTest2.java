bfe5e21f4798a6c7eca9b33a530a55e865ae71d8
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-25 15:14:01 +0900

    Create ApplicationContextBasicFindTest2.java

---
+++ 기본/4.3 스프링 빈 조회 - 기본/ApplicationContextBasicFindTest2.java
@@ -0,0 +1,61 @@
+⛓️‍💥test - beanfind - ApplicationContextBasicFindTest
+📌ApplicationContextBasicFindTest.java
+
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("이름 없이 타입으로만 조회")
+    void findBeanByType() {
+        MemberService memberService = ac.getBean(MemberService.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        assertThrows(NoSuchBeanDefinitionException.class, ()
+                -> ac.getBean("xxxxx", MemberService.class));
+    }
+}
+
+
+🚨 (에러 발생)
+@Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+        MemberService xxxxx = ac.getBean("xxxxx", MemberService.class); 
+}
+
+        /* 출력 결과 (에러 발생)
+        org.springframework.beans.factory.NoSuchBeanDefinitionException: No bean named 'xxxxx' available
+         */
