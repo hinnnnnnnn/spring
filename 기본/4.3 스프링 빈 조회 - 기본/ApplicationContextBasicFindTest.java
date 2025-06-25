d950dd986a82b20ff6ec8ee9416a72d419e58e7e
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-25 15:07:37 +0900

    Create ApplicationContextBasicFindTest.java

---
+++ 기본/4.3 스프링 빈 조회 - 기본/ApplicationContextBasicFindTest.java
@@ -0,0 +1,33 @@
+⛓️‍💥test - beanfind - ApplicationContextBasicFindTest
+📌ApplicationContextBasicFindTest.java
+
+package hello.core.beanfind;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ApplicationContextBasicFindTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+    @Test
+    @DisplayName("빈 이름으로 조회")
+    void findBeanByName() {
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+        System.out.println("memberService = " + memberService);
+        System.out.println("memberService.getClass() = " + memberService.getClass());
+    }
+}
+
+
+/* 출력 결과
+memberService = hello.core.member.MemberServiceImpl@23aa363a
+memberService.getClass() = class hello.core.member.MemberServiceImpl
+ */
