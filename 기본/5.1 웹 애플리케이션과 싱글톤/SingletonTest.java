277629fbfa8ccf74e35203b94d04c813a7a861d1
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-27 14:55:44 +0900

    Create SingletonTest.java

---
+++ 기본/5.1 웹 애플리케이션과 싱글톤/SingletonTest.java
@@ -0,0 +1,37 @@
+⛓️‍💥test - singleton - SingletonTest
+📌SingletonTest.java
+  
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("스프링 없는 순수한 DI 컨테이너")
+    void pureContainer() {
+        AppConfig appConfig = new AppConfig();
+
+        //1. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService1 = appConfig.memberService();
+
+        //2. 조회: 호출할 때 마다 객체를 생성
+        MemberService memberService2 = appConfig.memberService();
+
+        //참조값이 다른 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 =/= memberService2
+        Assertions.assertThat(memberService1).isNotSameAs(memberService2);
+    }
+}
+
+/* 출력 결과
+memberService1 = hello.core.member.MemberServiceImpl@130161f7
+memberService2 = hello.core.member.MemberServiceImpl@619713e5
+ */
