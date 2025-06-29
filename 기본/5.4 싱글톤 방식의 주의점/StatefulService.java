896c7789d0897b67b0bfd07149016c4596bc2b28
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-29 16:10:05 +0900

    Create StatefulService.java

---
+++ 기본/5.4 싱글톤 방식의 주의점/StatefulService.java
@@ -0,0 +1,19 @@
+⛓️‍💥test - singleton - StatefulService
+📌StatefulService.java
+  
+package hello.core.singleton;
+
+public class StatefulService {
+
+    private int price; //상태를 유지하는 필드
+
+    public void order(String name, int price) {
+        System.out.println("name = " + name + " price = " + price);
+        this.price = price; //여기가 문제!!!
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+}
