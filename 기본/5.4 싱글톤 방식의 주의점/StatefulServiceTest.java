ae5400859b9cc05fc4496730a08c9b303d5910d6
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-06-29 16:11:06 +0900

    Create StatefulServiceTest.java

---
+++ 기본/5.4 싱글톤 방식의 주의점/StatefulServiceTest.java
@@ -0,0 +1,49 @@
+⛓️‍💥test - singleton - StatefulServiceTest
+📌StatefulServiceTest.java
+
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService statefulService1 = ac.getBean(StatefulService.class);
+        StatefulService statefulService2 = ac.getBean(StatefulService.class);
+
+        //ThreadA: A사용자 10000원 주문
+        statefulService1.order("userA", 10000);
+        //ThreadB: B사용자 20000원 주문
+        statefulService2.order("userB", 20000);
+
+        //ThreadA: A사용자 주문 금액 조회
+        int price = statefulService1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(statefulService1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+
+}
+
+/* 출력 결과
+name = userA price = 10000
+name = userB price = 20000
+price = 20000
+*/
