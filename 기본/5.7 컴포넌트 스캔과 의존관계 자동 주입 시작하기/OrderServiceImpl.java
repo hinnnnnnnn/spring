c484f13174b70c6071e3042dfab8069782eb1d04
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-02 15:18:28 +0900

    Create OrderServiceImpl.java

---
+++ 기본/5.7 컴포넌트 스캔과 의존관계 자동 주입 시작하기/OrderServiceImpl.java
@@ -0,0 +1,36 @@
+📌OrderServiceImpl.java에 추가
+  
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+⭐️@Component⭐️
+public class OrderServiceImpl implements OrderService{
+
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    ⭐️@Autowired⭐️
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
+}
