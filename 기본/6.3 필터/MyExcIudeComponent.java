3cab5dbfb0274869f53be1cfc7441665ee6b2971
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-04 01:01:04 +0900

    Create MyExcIudeComponent.java

---
+++ 기본/6.3 필터/MyExcIudeComponent.java
@@ -0,0 +1,12 @@
+⛓️‍💥test - scan - filter - MyExcIudeComponent
+📌MyExcIudeComponent.java
+
+package scan.filter;
+
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface MyExcIudeComponent {
+}
