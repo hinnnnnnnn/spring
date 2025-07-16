bf1c07327daf0fb854af7a6abea5b5a6bfca5f23
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-16 15:01:49 +0900

    Create NetworkClient.java

---
+++ 기본/8.1 빈 생명주기 콜백/NetworkClient.java
@@ -0,0 +1,35 @@
+⛓️‍💥test - hello.core - lifecycle - NetworkClient
+📌NetworkClient.java
+  
+package hello.core.lifecycle;
+
+public class NetworkClient {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+}
+
+
