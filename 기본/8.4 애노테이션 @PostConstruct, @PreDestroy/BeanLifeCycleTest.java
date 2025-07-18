88e51c2c0c78ab1154120edd9b37829fe4d5e8d4
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-18 13:43:17 +0900

    Create BeanLifeCycleTest.java

---
+++ 기본/8.4 애노테이션 @PostConstruct, @PreDestroy/BeanLifeCycleTest.java
@@ -0,0 +1,38 @@
+BeanLifeCycleTest.java 수정
+  
+package hello.core.lifecycle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+public class BeanLifeCycleTest {
+
+    @Test
+    public void lifeCycleTest() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(LifeCycleConfig.class);
+        NetworkClient client = ac.getBean(NetworkClient.class);
+        ac.close();
+    }
+
+    @Configuration
+    static class LifeCycleConfig {
+
+        @Bean //원래대로 빈 등록
+        public NetworkClient networkClient() {
+            NetworkClient networkClient = new NetworkClient();
+            networkClient.setUrl("http://hello-spring.dev");
+            return networkClient;
+        }
+    }
+}
+
+/* 출력 결과
+생성자 호출, url = null
+connect: http://hello-spring.dev
+call: http://hello-spring.dev message = 초기화 연결 메시지
+close: http://hello-spring.dev
+ */
