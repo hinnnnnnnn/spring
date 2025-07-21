d9b575c6f8b32c5ffaf1e8bed5310416e4299921
hinnnnnnnn <144525925+hinnnnnnnn@users.noreply.github.com>
2025-07-21 17:15:14 +0900

    Create SingletonTest.java

---
+++ 기본/9.2 프로토타입 스코프/SingletonTest.java
@@ -0,0 +1,50 @@
+⛓️‍💥test - hello.core - scope - SingletonTest
+📌SingletonTest.java
+  
+package hello.core.scope;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test 
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+        SingletonBean singletonBean1 = ac.getBean(SingletonBean.class);
+        SingletonBean singletonBean2 = ac.getBean(SingletonBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        assertThat(singletonBean1).isSameAs(singletonBean2);
+
+        ac.close();
+
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void destroy() {
+            System.out.println("SingletonBean.destroy");
+        }
+    }
+}
+
+/* 출력 결과
+SingletonBean.init
+singletonBean1 = hello.core.scope.SingletonTest$SingletonBean@455b6df1
+singletonBean2 = hello.core.scope.SingletonTest$SingletonBean@455b6df1
+SingletonBean.destroy
+ */
